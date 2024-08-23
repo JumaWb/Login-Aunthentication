b7d0012b821bb0933b350fc85b4e78cd8a728253
Brian Juma <bjuma0985@gmail.com>
2024-08-24 01:20:08 +0300

    Third Commit

--- server/main.py
+++ server/main.py
@@ -2,47 +2,36 @@ from flask import Flask, render_template, request, redirect, session, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 
-
-
 app = Flask(__name__, template_folder='./template')
 app.secret_key = "your_secret_key"
 
-#configure SQL Alchemy
+# configure SQL Alchemy
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
-
-#Database Model
+# Database Model
 class User(db.Model):
-    #class variables
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(25), unique=True, nullable=False)
-    password = db.Column(db.String(150), nullable=False)
+    password_hash= db.Column(db.String(150), nullable=False)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
-        
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-        
-
-
-
-
-#Routes
+# Routes
 @app.route("/")
 def home():
     if "username" in session:
         return redirect(url_for("dashboard"))
     return render_template("index.html")
 
-#Login
+# Login
 @app.route("/login", methods=["POST"])
 def login():
-    #collect info from form
     username = request.form["username"]
     password = request.form["password"]
     user = User.query.filter_by(username=username).first()
@@ -51,26 +40,24 @@ def login():
         return redirect(url_for("dashboard"))
     else:
         return render_template("index.html")
-    
-    #Register
-    @app.route("/register", methods=["POST"])
-    def register():
-        username = request.form["username"]
-        password = request.form["password"]
-        user = User.query.filter_by(username=username).first()
-        if user:
-            return render_template("index.html", error="User already here!")
-        else:
-            new_user = User(username=username)
-            nuew_user.set_password(password)
-            db.session.add(new_user)
-            db.session.commit()
-            session["username"] = username
-            return redirect(url_for("dashboard"))
 
+# Register (Fixed indentation)
+@app.route("/register", methods=["POST"])
+def register():
+    username = request.form["username"]
+    password = request.form["password"]
+    user = User.query.filter_by(username=username).first()
+    if user:
+        return render_template("index.html", error="User already here!")
+    else:
+        new_user = User(username=username)
+        new_user.set_password(password)  # Fixed typo here as well (nuew_user -> new_user)
+        db.session.add(new_user)
+        db.session.commit()
+        session["username"] = username
+        return redirect(url_for("dashboard"))
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run(debug=True)
-
